6fc76b781324998a40a155a5ecccb6687612bb8b
gaétan-lemont <gaetanl2004@gmail.com>
2025-05-21 17:20:55 +0200

    Remove auto generated useless fnction

--- src/main/java/com/mycompany/covoitme/ServletListCreatedPath.java
+++ src/main/java/com/mycompany/covoitme/ServletListCreatedPath.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.*;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -20,33 +19,6 @@ import java.util.Date;
  */
 @WebServlet(name = "ServletListCreatedPath", urlPatterns = { "/createdPath" })
 public class ServletListCreatedPath extends HttpServlet {
-
-  /**
-   * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-   * methods.
-   *
-   * @param request servlet request
-   * @param response servlet response
-   * @throws ServletException if a servlet-specific error occurs
-   * @throws IOException if an I/O error occurs
-   */
-  protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-    response.setContentType("text/html;charset=UTF-8");
-    try (PrintWriter out = response.getWriter()) {
-      /* TODO output your page here. You may use following sample code. */
-      out.println("<!DOCTYPE html>");
-      out.println("<html>");
-      out.println("<head>");
-      out.println("<title>Servlet ServletListCreatedPath</title>");
-      out.println("</head>");
-      out.println("<body>");
-      out.println("<h1>Servlet ServletListCreatedPath at " + request.getContextPath() + "</h1>");
-      out.println("</body>");
-      out.println("</html>");
-    }
-  }
-
   /**
    * Handles the HTTP <code>GET</code> method.
    *
