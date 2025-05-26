4543ebbf7107b4b9bb5a5bc57d8de22745bb8063
gaétan-lemont <gaetanl2004@gmail.com>
2025-05-26 18:11:07 +0200

    Remove auto generated useless fnction

--- src/main/java/com/mycompany/covoitme/ServletListMyPath.java
+++ src/main/java/com/mycompany/covoitme/ServletListMyPath.java
@@ -24,33 +24,6 @@ import java.util.Date;
  */
 @WebServlet(name = "ServletMyPath", urlPatterns = { "/mypath" })
 public class ServletListMyPath extends HttpServlet {
-
-  /**
-   * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-   * methods.
-   *
-   * @param request servlet request
-   * @param response servlet response
-   * @throws ServletException if a servlet-specific error occurs
-   * @throws IOException if an I/O error occurs
-   */
-  protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-    response.setContentType("text/html;charset=UTF-8");
-    try (PrintWriter out = response.getWriter()) {
-      /* TODO output your page here. You may use following sample code. */
-      out.println("<!DOCTYPE html>");
-      out.println("<html>");
-      out.println("<head>");
-      out.println("<title>Servlet ServletMyPath</title>");
-      out.println("</head>");
-      out.println("<body>");
-      out.println("<h1>Servlet ServletMyPath at " + request.getContextPath() + "</h1>");
-      out.println("</body>");
-      out.println("</html>");
-    }
-  }
-
   /**
    * Handles the HTTP <code>GET</code> method.
    *
@@ -128,20 +101,6 @@ public class ServletListMyPath extends HttpServlet {
     }
 
     request.getRequestDispatcher("/myPath.jsp").forward(request, response);
-  }
-
-  /**
-   * Handles the HTTP <code>POST</code> method.
-   *
-   * @param request servlet request
-   * @param response servlet response
-   * @throws ServletException if a servlet-specific error occurs
-   * @throws IOException if an I/O error occurs
-   */
-  @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-    processRequest(request, response);
   }
 
   /**
